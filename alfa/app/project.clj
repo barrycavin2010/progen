920d8738312d41b3d94c833cbe48f18b1b1705f3
Sabda PS <sabdaps@gmail.com>
2020-10-27 21:30:15 +0700

    Update project.clj

--- alfa/app/project.clj
+++ alfa/app/project.clj
@@ -2,40 +2,40 @@
   :dependencies
   [[org.clojure/clojure "1.8.0"]
    [org.clojure/clojurescript "1.9.93"]
-   [com.stuartsierra/component "0.3.1"]
+   [com.stuartsierra/component "1.0.0"]
 
    ;; html
-   [selmer "1.0.7" :exclusions [commons-codec]]
+   [selmer "1.12.31" :exclusions [commons-codec]]
 
    ;; cljs
-   [reagent "0.6.0-alpha2" :exclusions [org.clojure/tools.reader cljsjs/react]]
-   [re-frame "0.7.0" :exclusions [reagent cljsjs/react]]
-   [cljs-ajax "0.5.8" ]
-   [cljs-react-material-ui "0.2.19" ]
+   [reagent "0.10.0" :exclusions [org.clojure/tools.reader cljsjs/react]]
+   [re-frame "1.1.1" :exclusions [reagent cljsjs/react]]
+   [cljs-ajax "0.8.1" ]
+   [cljs-react-material-ui "0.2.50" ]
    ;;:exclusions [cljsjs/react]
    ;;[cljsjs/react-with-addons "15.1.0-0"]
 
    ;; basic web setup
-   [compojure "1.5.1" :exclusions [commons-codec]]
-   [org.immutant/web "2.1.5"
+   [compojure "1.6.2" :exclusions [commons-codec]]
+   [org.immutant/web "2.1.10"
     :exclusions [commons-codec]]
    [lib-noir "0.9.9" :exclusions [commons-codec]]
-   [ring/ring-defaults "0.2.1" :exclusions [commons-codec]]
-   [ring "1.5.0" :exclusions [commons-codec]]
-   [com.taoensso/carmine "2.13.1" :exclusions [com.taoensso/encore]]
+   [ring/ring-defaults "0.3.2" :exclusions [commons-codec]]
+   [ring "1.8.2" :exclusions [commons-codec]]
+   [com.taoensso/carmine "3.0.1" :exclusions [com.taoensso/encore]]
 
    ;; standard web utilities
-   [com.taoensso/timbre "4.6.0"]
-   [environ "1.0.3"]
+   [com.taoensso/timbre "5.1.0"]
+   [environ "1.2.0"]
 
    ;; file/formatting and development utilities
-   [org.clojure/tools.namespace "0.2.11"]
-   [dk.ative/docjure "1.10.0"]
-   [danlentz/clj-uuid "0.1.6"]
+   [org.clojure/tools.namespace "1.0.0"]
+   [dk.ative/docjure "1.14.0"]
+   [danlentz/clj-uuid "0.1.9"]
    [me.raynes/fs "1.4.6"]
-   [prismatic/schema "1.1.2"]
-   [pjstadig/humane-test-output "0.8.0"]
-   [ring/ring-mock "0.3.0"]]
+   [prismatic/schema "1.1.12"]
+   [pjstadig/humane-test-output "0.10.0"]
+   [ring/ring-mock "0.4.0"]]
 
   :injections [(require 'pjstadig.humane-test-output)
                (pjstadig.humane-test-output/activate!)]
@@ -50,8 +50,8 @@
 
   :repl-options {:init-ns user}
 
-  :plugins [[lein-cljsbuild "1.1.3"]
-            [lein-cloverage "1.0.7-SNAPSHOT"]]
+  :plugins [[lein-cljsbuild "1.1.8"]
+            [lein-cloverage "1.2.1"]]
 
   :cljsbuild
   {:test-commands
