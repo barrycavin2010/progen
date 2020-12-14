b6a293e08e14f0fb716d41a46c80db8184de9d69
Sabda PS <sabdaps@gmail.com>
2020-12-15 05:40:37 +0700

    Update dbase.clj

--- site-alfa/app/src/clj/app/logic/dbase.clj
+++ site-alfa/app/src/clj/app/logic/dbase.clj
@@ -3,15 +3,48 @@
     [com.stuartsierra.component :as component]
     [app.utils :refer :all]))
 
+(declare grab-content init-db set-cronjob load-db)
+
 (defrecord Dbase [source redis]
   component/Lifecycle
   (start [this]
     (println "Dbase component started")
-    (assoc this :templates {:pesan "Amitabacan"}))
+    (let [db (ref nil)]
+      (init-db source db)
+      (load-db db redis)
+      (set-cronjob db redis)
+      (merge this
+             {:templates {:pesan "Amitabacan"}
+              :conn redis
+              :db db})))
   (stop [this] this))
 
 (defn make [db-config]
   (map->Dbase db-config))
+
+(defn init-db
+  [source db]
+  (dosync (ref-set db {:content {:template-math {}
+                                 :template-english {}
+                                 :template-logic {}
+                                 :data-math {}
+                                 :data-english {}
+                                 :data-logic}
+                       :user {:profile {}
+                              :stat-math {}
+                              :stat-logic {}
+                              :stat-english {}
+                              :stat-template-math {}
+                              :stat-template-logic {}
+                              :stat-template-english {}}
+                       :stat {:template-math {}
+                              :template-english {}
+                              :template-logic {}
+                              :data-math {}
+                              :data-english {}
+                              :data-logic}})))
+
+
 
 
 
